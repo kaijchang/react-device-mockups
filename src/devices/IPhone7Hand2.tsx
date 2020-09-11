91f7b61765b1ab5c856905de7ab43f26ff22694f
Kai Chang <kaijchang@gmail.com>
2020-09-11 11:15:25 -0700

    add unsaved files

--- src/devices/IPhone7Hand2.tsx
+++ src/devices/IPhone7Hand2.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 
-import Device from '../Device';
+import Device, { Props as DeviceProps } from '../Device';
 
-export type Props = {
-    buttonProps: React.HTMLProps<HTMLDivElement>,
-    screenProps: React.HTMLProps<HTMLDivElement>,
-    width?: number,
-    height?: number
-};
+export type Props = Omit<Omit<DeviceProps, 'color'>, 'orientation'>;
 
 export default class IPhone7Hand2 extends React.Component<Props> {
     render() {
